a1d862d3b7b81df9122964cd57991aa6b7a9c059
julianlinux <julianlinux@julianlinux-X555LA>
2015-10-05 18:18:11 +0200

    FINAL

---
+++ src/bol22/Bol22.java
@@ -0,0 +1,27 @@
+
+package bol22;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author julianlinux
+ */
+public class Bol22 {
+
+    
+    public static void main(String[] args) {
+        float lado;
+        Scanner obx=new Scanner (System.in);
+        System.out.println("Teclea un lado");
+        lado=obx.nextFloat();
+        System.out.println("Mete el otro lado");
+        lado=obx.nextFloat();
+        System.out.println("area="+lado*lado);
+        
+        
+        
+        
+    }
+    
+}
